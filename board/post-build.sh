2760d45ef7363f7ee35a8024525acfaca9234e6f
hi0t <27971344+hi0t@users.noreply.github.com>
2019-04-28 23:13:20 +0300

    fix gpio permissions

--- board/post-build.sh
+++ board/post-build.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-test -d $TARGET_DIR/etc/sudoers.d || mkdir $TARGET_DIR/etc/sudoers.d
-test -f $TARGET_DIR/etc/sudoers.d/ab || \
-    echo "ab ALL = ALL" >$TARGET_DIR/etc/sudoers.d/ab; \
-    chmod 0440 $TARGET_DIR/etc/sudoers.d/ab
+mkdir -p $TARGET_DIR/etc/sudoers.d
+echo "ab ALL = ALL" > $TARGET_DIR/etc/sudoers.d/ab
+chmod 0440 $TARGET_DIR/etc/sudoers.d/ab
+
+echo "" >> $TARGET_DIR/etc/mdev.conf
+echo "# Raspberry Pi devices" >> $TARGET_DIR/etc/mdev.conf
+echo "gpiomem	root:gpio 660" >> $TARGET_DIR/etc/mdev.conf
