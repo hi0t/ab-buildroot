9ec31f926c2e5e473644cbd58ff2e91340dcb030
hi0t <27971344+hi0t@users.noreply.github.com>
2019-04-29 00:19:05 +0300

    enable wifi

--- board/post-build.sh
+++ board/post-build.sh
@@ -6,6 +6,18 @@ mkdir -p $TARGET_DIR/etc/sudoers.d
 echo "ab ALL = ALL" > $TARGET_DIR/etc/sudoers.d/ab
 chmod 0440 $TARGET_DIR/etc/sudoers.d/ab
 
-echo "" >> $TARGET_DIR/etc/mdev.conf
-echo "# Raspberry Pi devices" >> $TARGET_DIR/etc/mdev.conf
-echo "gpiomem	root:gpio 660" >> $TARGET_DIR/etc/mdev.conf
+(
+    echo ""
+    echo "# Raspberry Pi devices"
+    echo "gpiomem	root:gpio 660"
+) >> $TARGET_DIR/etc/mdev.conf
+
+(
+    echo ""
+    echo "auto wlan0"
+    echo "iface wlan0 inet dhcp"
+    echo "  pre-up wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant.conf"
+    echo "  post-down killall -q wpa_supplicant"
+    echo "  wait-delay 15"
+    echo "  hostname \$(hostname)"
+) >> $TARGET_DIR/etc/network/interfaces
